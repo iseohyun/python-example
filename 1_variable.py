7540727b491d55a9945cc0a17d1630bb384a95dc
iseohyun <iseohyun@hanmail.net>
2021-09-18 08:49:04 +0900

    make git repo

---
+++ 1_variable.py
@@ -0,0 +1,14 @@
+print("우리집 개 이름은 연탄이예요")
+print("연탄이는 4살이며, 산책을 아주 좋아해요")
+print("연탄이는 어른일까요? true")
+
+==================================================
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age>=3
+
+print("우리집 "+ animal +" 이름은 "+ name +"예요")
+print(name+"는 "+ str(age) +"살이며, "+hobby+"을 아주 좋아해요")
+print(name+"는 어른일까요? "+ str(is_adult))
